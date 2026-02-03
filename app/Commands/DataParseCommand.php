6653cf75bc673da6fd9c32542406dd62ee10cf57
brendt <brent.roose@gmail.com>
2026-02-03 09:04:40 +0100

    wip

--- app/Commands/DataParseCommand.php
+++ app/Commands/DataParseCommand.php
@@ -5,7 +5,6 @@ namespace App\Commands;
 use App\Parser;
 use Tempest\Console\ConsoleCommand;
 use Tempest\Console\HasConsole;
-use function Tempest\env;
 
 final class DataParseCommand
 {
@@ -15,6 +14,7 @@ final class DataParseCommand
     public function __invoke(
         string $inputPath = __DIR__ . '/../../data/data.csv',
         string $outputPath = __DIR__ . '/../../data/data.json',
+        bool $store = false,
     ): void {
         $startTime = microtime(true);
 
@@ -26,9 +26,13 @@ final class DataParseCommand
         $branchName = exec('git branch --show-current');
 
         $leaderBoardEntry = time() . ';' . $branchName . ';' . $executionTime;
-        $leaderBoardFile = fopen(__DIR__ . '/../../leaderboard.csv', 'a');
-        fwrite($leaderBoardFile, $leaderBoardEntry . PHP_EOL);
-        fclose($leaderBoardFile);
+
+        if ($store) {
+            $leaderBoardFile = fopen(__DIR__ . '/../../leaderboard.csv', 'a');
+            fwrite($leaderBoardFile, $leaderBoardEntry . PHP_EOL);
+            fclose($leaderBoardFile);
+            $this->success('Written to leaderboard.csv');
+        }
 
         $this->success($leaderBoardEntry);
     }
